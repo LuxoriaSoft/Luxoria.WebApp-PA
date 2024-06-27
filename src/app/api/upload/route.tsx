2b2a669cf976ca808423e7b8da56f03dfd273599
CPU64x <q.erdinger@orange.fr>
2024-06-27 14:56:24 +0200

    Fix: POST upload route

--- src/app/api/upload/route.tsx
+++ src/app/api/upload/route.tsx
@@ -1,17 +1,22 @@
-import {NextApiRequest, NextApiResponse} from "next";
+import {NextRequest, NextResponse} from "next/server";
 
-export async function GET(req: NextApiRequest) {
+export async function POST(request: NextRequest) {
+    const data = await request.formData();
+    const file: File | null = data.get("file") as File;
 
-    return new Response(
-        JSON.stringify({
-            message: "Hello World",
-        }
-    ), {
-        status: 201,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
+    if (!file) {
+        return NextResponse.json({error: "No file uploaded"}, {status: 400});
+    }
 
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
 
+    console.log(buffer);
+
+    return NextResponse.json({size: buffer.length});
 }
+
+/*
+curl -X POST http://localhost:3000/api/upload \
+  -F "file=@/Users/quentin/Downloads/accept.png"
+ */
