d6f57b4398c538dc6772a26ae3e9201e63b0ece7
CPU64x <q.erdinger@orange.fr>
2024-06-28 09:50:40 +0200

    Add: grid system to display card 2 by 2

--- src/components/picture.tsx
+++ src/components/picture.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 export default function CardComponent({ _id, name }) {
+
   return (
     <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
       <div className="px-4 py-5 sm:px-6">
@@ -10,17 +11,17 @@ export default function CardComponent({ _id, name }) {
         <Image
           src={`/api/preview?_id=${_id}`}
           alt={name}
-          objectFit={"contain"}
-          layout={"fill"}
+          objectFit="cover"
+          layout="fill"
         />
       </div>
       <div className="px-4 py-4 sm:px-6">
-        <button
-          type="button"
+        <a
           className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          href={`/api/download?_id=${_id}`}
         >
           Download
-        </button>
+        </a>
       </div>
     </div>
   );
