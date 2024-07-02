a83c106dea8507d980e4cf7ed55a53fb8e075db3
CPU64x <q.erdinger@orange.fr>
2024-07-02 20:29:21 +0200

    Add: mail service && send mail on every gallery creation

--- src/app/api/galleries/create/route.tsx
+++ src/app/api/galleries/create/route.tsx
@@ -1,11 +1,16 @@
 import {NextRequest, NextResponse} from "next/server";
 import {getDbInstance} from "@/lib/services/database.service";
+import MailerService from "@/lib/services/mailer.service";
+import {ObjectId} from "mongodb";
 
 interface Gallery {
+  _id: ObjectId;
   name: string;
   description: string;
   email: string;
 }
+
+const mailer = new MailerService();
 
 export async function POST(request: NextRequest) : Promise<void | Response> {
   try {
@@ -14,6 +19,9 @@ export async function POST(request: NextRequest) : Promise<void | Response> {
 
     // Insert gallery into 'galleries' collection
     await db.collection("galleries").insertOne(body);
+
+    // Send email notification
+    await mailer.sendEmail(body);
 
     return NextResponse.json({ success: "Gallery created successfully!", body  }, { status: 200 });
 
